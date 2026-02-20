00561d97926e99260dcba83cb7fb7cddc604234f
MonsieurTC <99206721+MonsieurTC@users.noreply.github.com>
2026-02-20 11:50:38 +0100

    Add initial JavaScript learning portfolio script

---
+++ script.js
@@ -0,0 +1,9 @@
+// JS Learning Portfolio
+// Chaque jour, ajouter le code correspondant à la section du jour
+
+// Exemple Jour 1 : Variables & Types
+let nom = "Richard";
+const age = 46;
+var pays = "France";
+
+console.log(nom, age, pays);
